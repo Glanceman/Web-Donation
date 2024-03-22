b1e91f6ca37df31f2409cf2ec42122b8a63a60f8
Ben <benxian456@gmail.com>
2024-03-22 16:26:28 +0800

    sample code for create maybe?

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>Create</div>
+  <div>
+    <el-button type="primary" @click="createPost"> Create sample board </el-button>
+  </div>
 </template>
 <script>
+import Web3Service from '@/web3Service'
 export default {
   data() {
-    return {}
+    return {
+      account: null,
+      storageInst: null
+    }
   },
-  methods: {},
-  mounted() {}
+  methods: {
+    async createPost() {
+      // sample only
+      console.log(this.storageInst.methods)
+      console.log(this.account)
+      let resp = await this.storageInst.methods
+        .createBoard(
+          'testname',
+          String(this.account),
+          'PostTitle',
+          'PostContext',
+          123456 // target amount
+        )
+        .send()
+      // no complete
+      //send() pay gas free call() no need reference link :https://bitsofco.de/calling-smart-contract-functions-using-web3-js-call-vs-send/
+
+      console.log(resp)
+    }
+  },
+  async mounted() {
+    await Web3Service.getInstance().getCurrentConnectedAccount()
+    this.account = Web3Service.getInstance().account
+    this.storageInst = Web3Service.getInstance().getStorageContract()
+  }
 }
 </script>
 <style></style>
