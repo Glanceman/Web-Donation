2244fc6a20610093da62cc202357ead7f13a7531
Ben <benxian456@gmail.com>
2024-04-14 16:37:52 +0800

    update the base url

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Web-Donation/", /* This only for github page repo name*/
   plugins: [
     vue(),
   ],
