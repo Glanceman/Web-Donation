77cd807716e2e8ee14265e78e74177d92976caa4
Ben <benxian456@gmail.com>
2024-04-15 20:53:51 +0800

    change router mode to hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import PostView from '@/views/PostView.vue'
 import CreateView from '@/views/CreateView.vue'
 import HistoryView from '@/views/HistoryView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
